f61ae6d102b66346a5ad6d5841da8fd7457eb65a
Aya Abdul Nabi <ayaabdulnabi04@gmail.com>
2025-11-27 22:36:27 -0500

    works

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import Tilt from 'react-parallax-tilt';
+import { motion } from 'framer-motion';
+
+import { styles } from '../style';
+import { github } from '../assets';
+import { SectionWrapper } from '../hoc';
+import { projects } from '../constants';
+import { fadeIn, textVariant } from '../utils/motion';
+
+
 
 const Works = () => {
   return (
-    <div>Works</div>
+    <>
+      <motion.div variants={textVariant()}>
+          <p className={styles.sectionSubText}>My work </p>
+          <h2 className={styles.sectionHeadText}> Projects.</h2>
+      </motion.div>
+
+      <div className='w-full flex'>
+        <motion.p
+          variants={fadeIn("", "", 0.1, 1)}
+          className='mt-3 text-secondary text-[17px] max-w-3xl leading [30px]'
+        >
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+        incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
+        exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor 
+        in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint 
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </motion.p>
+      </div>
+      <div className='mt-20 flex flex-wrap gap-7'>
+
+      </div>
+    </>
   )
 }
 
-export default Works
+export default SectionWrapper(Works, "");
